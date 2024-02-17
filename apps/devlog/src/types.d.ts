ef020ee794666ec3a2ad014829f047cce5d19067
HoJin9622 <kiss0104040@gmail.com>
2024-02-17 16:45:19 +0900

    feat: 파일 제거

--- apps/devlog/src/types.d.ts
+++
@@ -1,9 +0,0 @@
-type Post = {
-  title: string;
-  date: string;
-  subtitle: string;
-  category: string;
-  slug: string;
-  content: string;
-  coverImage?: string;
-};
--- apps/devlog/src/utils/textTransformer.ts
+++
@@ -1,5 +0,0 @@
-function textTransformer(text: string) {
-  return text.replaceAll(' ', '-').toLowerCase();
-}
-
-export default textTransformer;
