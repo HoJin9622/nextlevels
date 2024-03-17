0da749a6224aa782a12b3381dc06628a38d00a1d
HoJin9622 <kiss0104040@gmail.com>
2024-03-17 22:04:23 +0900

    feat: 개인정보처리방침 디자인 변경

--- src/app/seoul-cultural-events/privacy/page.tsx
+++ src/app/seoul-cultural-events/privacy/page.tsx
@@ -1,10 +1,10 @@
 export default function Privacy() {
   return (
-    <>
-      <h1>개인정보처리방침</h1>
+    <section className='px-5 py-4'>
+      <h1 className='text-2xl font-bold'>개인정보처리방침</h1>
       <p>
         서울 문화행사 app does not currently collect any personal information.
       </p>
-    </>
+    </section>
   )
 }
