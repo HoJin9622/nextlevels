30e5d9f2d908371ac7e2f4b19b1230967cefacce
HoJin9622 <kiss0104040@gmail.com>
2024-03-17 21:31:52 +0900

    feat: 서울 문화행사 개인정보 처리방침 작성

---
+++ src/app/seoul-cultural-events/privacy/page.tsx
@@ -0,0 +1,10 @@
+export default function Privacy() {
+  return (
+    <>
+      <h1>개인정보처리방침</h1>
+      <p>
+        서울 문화행사 app does not currently collect any personal information.
+      </p>
+    </>
+  )
+}
